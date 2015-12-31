69b8e0f45002f0021308a170360191c5a21e1a1b
gumbyferret <yb@converged-systems.com>
2015-12-31 00:52:12 +0000

    Update receive.js

--- receive.js
+++ receive.js
@@ -1,14 +1,34 @@
-var http = require('http'),
-    twilio = require('twilio');
+var http = require('http');
+var twilio = require('twilio');
+var qs = require('querystring');
 
 http.createServer(function (req, res) {
-    //Create TwiML response
-    var twiml = new twilio.TwimlResponse();
-    twiml.say('Hello World!');
 
+  var body = '';
+
+  req.setEncoding('utf8');
+
+  req.on('data', function(data) {
+    body += data;
+  });
+
+  req.on('end', function() {
+    var data = qs.parse(body);
+    var twiml = new twilio.TwimlResponse();
+    var jsonString = JSON.stringify(data);
+    var jsonDataObject = JSON.parse(jsonString);
+    twiml.say('Hello there')
+    .pause({ length:3 })
+    .say('How are you today?', {
+        voice:'woman',
+        language:'en-gb'
+    });
+    //.play('http://www.example.com/some_sound.mp3');
+
+    twiml.message('Thanks, your message of ' + jsonDataObject.Body + ' was received!');
+    console.log('Message from: '+jsonDataObject.From+' saying: '+jsonDataObject.Body);
     res.writeHead(200, {'Content-Type': 'text/xml'});
     res.end(twiml.toString());
+  });
 
-}).listen(1337, '52.48.3.81');
-
-console.log('TwiML servin\' server running at http://52.48.3.81:1337/');
+}).listen(80);
